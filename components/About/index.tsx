fd2f1f200808db5458389236b705373dfb63827c
SAKSHI BHATIA <145453542+Sakshibhatia06@users.noreply.github.com>
2025-10-23 09:20:09 +0530

    Update index.tsx

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -50,7 +50,7 @@ const About = () => {
             >
               <h2 className="mb-5 text-3xl font-bold text-black dark:text-white">
                 Why Students Choose{" "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                <span className="relative inline-block">
                   Assignment Wallah
                 </span>{" "}
                 for Homework Support?
@@ -98,7 +98,7 @@ const About = () => {
 
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 We Prioritize Your Academic Needs{" "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
+                <span className="relative inline-block ">
                   Within Budget
                 </span>
               </h2>
@@ -165,4 +165,6 @@ const About = () => {
 };
 
 export default About;
+
+
 
